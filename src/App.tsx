2580c347a118ed8f40338720e464a5bcf8238241
Gustavo Constantino <gustavohconstantino@outlook.com>
2023-06-30 19:55:40 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./style/global.css";
+
+export function App() {
+  return (
+    <>
+      <h1 className="font-bold">Hello World</h1>
+      <button className="bg-violet-500 font-medium px-4 py-2 rounded text-white hover:bg-violet-700">
+        Enviar
+      </button>
+    </>
+  );
+}
