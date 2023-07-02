b43b9b264d0386cbab39d33ee181ac9a4eb54b48
Gustavo Constantino <gustavohconstantino@outlook.com>
2023-07-01 23:55:17 -0300

    fix

--- src/components/Test/Text.stories.tsx
+++ src/components/Test/Text.stories.tsx
@@ -22,4 +22,20 @@ export const Default: StoryObj<TestProps> = {};
 // Para habilitar mais variações
 // export const Medium: StoryObj<TestProps> = {};
 // export const Small: StoryObj<TestProps> = {};
-// export const CustomComponent: StoryObj<TestProps> = {};
+
+export const CustomComponent: StoryObj<TestProps> = {
+  args: {
+    asChild: true,
+    TextContent: <p className="text-Primary500">Eu sou um paragráfo</p>,
+  },
+  argTypes: {
+    TextContent: {
+      // control: {
+      //   type: null,
+      // },
+      table: {
+        disable: true,
+      },
+    },
+  },
+};
