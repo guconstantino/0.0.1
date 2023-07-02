f1872c6fbe3a01880101bb60413e0932014693a3
Gustavo Constantino <gustavohconstantino@outlook.com>
2023-07-02 20:22:48 -0300

    Adicionado Tokens

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App.tsx";
+import * as body from "./style/globalVariables/textStyles/body.tsx";
 
 //Import Components
 // import { Test } from "./components/Avatar/Avatar.tsx";
@@ -10,5 +11,8 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <App />
     <br />
     {/* Components */}
+    <body.Regular />
+    <body.Medium />
+    <body.Bold />
   </React.StrictMode>
 );
